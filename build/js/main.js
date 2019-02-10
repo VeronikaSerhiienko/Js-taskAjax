2666f7b1e1c728a509581029682766a302600c63
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2019-02-10 22:07:54 +0200

    fix README file

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+import {FirstRequest} from "./FirstRequest";
+import {CardsRequest} from "./Cards";
+import {VisitorsRequest} from "./VisitorsRequest";
+import {UsersRequest} from "./UsersRequest";
+FirstRequest();
+CardsRequest();
+VisitorsRequest();
+UsersRequest();
